d213055b5fc47a4bc0c4eb0af9615f46f792548e
spencer-weaver <sweavertheslayr@outlook.com>
2025-02-11 21:15:55 +0000

    fixed errors

--- src/perceptron.c
+++ src/perceptron.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "perceptron.h"
 
 // random_weights(p) sets all of the weights of p to random values 0-1
 // requires: p is not null
 void random_weights(perceptron *p) {
   for (int i = 0; i < p->input_amount; i++) {
-    p->weights[i] = float(rand() % 100) / 100;
+    p->weights[i] = rand() % 100 / 100;
   }
 }
 
 perceptron * init_perceptron(int input_amount, float bias) {
   perceptron *p = malloc(sizeof(perceptron));
   p->input_amount = input_amount;
-  p->weights = malloc(inputs * sizeof(float));
+  p->weights = malloc(input_amount * sizeof(float));
   p->bias = bias;
   return p;
 }
